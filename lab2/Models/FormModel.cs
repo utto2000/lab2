14ef72871256e8c1b3425ee442d1283c3eafa764
utto2000 <bartek200039@gmail.com>
2022-01-15 14:40:31 +0100

    Add files via upload

---
+++ lab2/Models/FormModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OData.Edm;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace lab2.Models
+{
+
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class FormModel
+    {
+        [Display(Name = "Nazwa elementu")]
+        public string Name { get; set; }
+        [Display(Name = "date of birth")]
+        [DataType(DataType.Date)]
+        public DateTime date_of_birth { get; set; }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
